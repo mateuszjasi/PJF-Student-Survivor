860a846ba3f86df241119560b9fa57d7b7da4f39
Mateusz <mateuszjasi@op.pl>
2022-12-14 18:05:13 +0100

    idk comment or smth

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1600, 900))
+screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption('Jeszcze nie wiem')
 while True:
     for event in pygame.event.get():
