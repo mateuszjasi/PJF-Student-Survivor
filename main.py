23e69e95612f1cb02e323ae3b41a1b6373545188
Mateusz <mateuszjasi@op.pl>
2022-12-14 18:03:25 +0100

    a

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import pygame
+from sys import exit
 
 pygame.init()
+screen = pygame.display.set_mode((1600, 900))
+pygame.display.set_caption('Jeszcze nie wiem')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
