2701c83441d018d0528366d0161688fa6ca225ac
Mateusz <mateuszjasi@op.pl>
2022-12-16 18:32:31 +0100

    idk comment or smth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from sys import exit
 from random import randint
 
@@ -26,19 +27,26 @@ class Player(pygame.sprite.Sprite):
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, type):
+    def __init__(self):
         super().__init__()
-        if type:
-            pass
         self.image = pygame.image.load('graphics/enemy.png').convert()
         self.rect = self.image.get_rect(center=(randint(-100, 1700), randint(-100, 1000)))
+        self.movement_speed = 1.5
         # if randint(0, 2):
         #    self.rect = self.image.get_rect(center=(-50, randint(0, 1000)))
         # else:
-        #     self.rect = self.image.get_rect(center=(randint(0, 1600), -50))
+        #    self.rect = self.image.get_rect(center=(randint(0, 1600), -50))
+
+    def move_toward_player(self):
+        dx, dy = player.sprite.rect.centerx - self.rect.centerx, player.sprite.rect.centery - self.rect.centery
+        dist = math.hypot(dx, dy)
+        if dist:
+            dx, dy = dx / dist, dy / dist
+            self.rect.x += dx * self.movement_speed
+            self.rect.y += dy * self.movement_speed
 
     def update(self):
-        pass
+        self.move_toward_player()
 
 
 def enemy_hit():
@@ -47,6 +55,27 @@ def enemy_hit():
         player.remove()
         return False
     return True
+
+
+def prevent_overlap():
+    pass
+    # need adjustment
+    # for enemy1 in enemies:
+    #    for enemy2 in enemies:
+    #        if enemy1 != enemy2:
+    #            if enemy1.rect.colliderect(enemy2.rect):
+    #                if enemy1.rect.bottom > enemy2.rect.top:
+    #                    overlap = enemy1.rect.bottom - enemy2.rect.top
+    #                    enemy1.rect.bottom -= overlap
+    #                if enemy1.rect.top < enemy2.rect.bottom:
+    #                    overlap = enemy2.rect.bottom - enemy1.rect.top
+    #                    enemy1.rect.top += overlap
+    #                if enemy1.rect.left < enemy2.rect.right:
+    #                    overlap = enemy1.rect.left - enemy2.rect.right
+    #                    enemy1.rect.left -= overlap
+    #                if enemy1.rect.right > enemy2.rect.left:
+    #                    overlap = enemy2.rect.left - enemy1.rect.right
+    #                    enemy1.rect.right += overlap
 
 
 pygame.init()
@@ -63,7 +92,7 @@ enemies = pygame.sprite.Group()
 game_message = test_font.render('Press space to run', False, 'black')
 game_message_rect = game_message.get_rect(center=(800, 600))
 
-# enemy spawn timer
+# Enemy spawn timer
 spawn_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(spawn_timer, 1500)
 
@@ -75,7 +104,7 @@ while True:
         if game_active:
             if event.type == spawn_timer:
                 # spawn enemy
-                enemies.add(Enemy('???'))
+                enemies.add(Enemy())
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 # game start
@@ -89,7 +118,8 @@ while True:
         player.update()
         enemies.draw(screen)
         enemies.update()
-        game_active = enemy_hit()
+        prevent_overlap()
+        # game_active = enemy_hit()
     else:
         # menu
         screen.fill('lightgrey')
