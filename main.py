a3d07801aa3aa2234a60391a39594134a0310569
Mateusz <mateuszjasi@op.pl>
2022-12-17 22:05:08 +0100

    idk comment or smth

--- main.py
+++ main.py
@@ -1,17 +1,104 @@
 import random
-
 import pygame
+import math
 from sys import exit
 from random import randint
+
+pygame.init()
+screen = pygame.display.set_mode((1600, 900))
+pygame.display.set_caption('Dzikie fotele w twojej okolicy')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
+# Intro screen
+game_message = test_font.render('Press space to run', False, 'black')
+game_message_rect = game_message.get_rect(center=(800, 600))
+
+
+def enemy_hit():
+    if pygame.sprite.spritecollide(player.sprite, enemies, False):
+        enemies.empty()
+        bullets.empty()
+        player.remove()
+        return False
+    return True
+
+
+class PlayerBullet(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.x = player.sprite.rect.centerx
+        self.y = player.sprite.rect.centery
+        self.speed = 10
+        self.angle = math.atan2(self.y - mouse_y, self.x - mouse_x)
+        self.x_vel = math.cos(self.angle) * self.speed
+        self.y_vel = math.sin(self.angle) * self.speed
+        self.image = pygame.Surface([10, 10])
+        self.image.fill((255, 255, 255))
+        self.image.set_colorkey((255, 255, 255))
+        pygame.draw.circle(self.image, (0, 0, 0), (self.image.get_width() / 2, self.image.get_height() / 2), 5)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def move(self):
+        self.rect.x -= int(self.x_vel)
+        self.rect.y -= int(self.y_vel)
+
+    def check_outside(self):
+        if self.rect.left > screen.get_width() or self.rect.right < 0:
+            self.kill()
+        if self.rect.top > screen.get_height() or self.rect.bottom < 0:
+            self.kill()
+
+    def update(self):
+        self.move()
+        self.check_outside()
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.player_stand = pygame.image.load('graphics/player.png').convert_alpha()
-        self.image = self.player_stand
-        self.rect = self.player_stand.get_rect(center=(800, 450))
+        self.walking_animation = [pygame.image.load("graphics/player_walk_0.png"),
+                                  pygame.image.load("graphics/player_walk_1.png"),
+                                  pygame.image.load("graphics/player_walk_2.png"),
+                                  pygame.image.load("graphics/player_walk_3.png")]
+        self.player_weapon = pygame.image.load("graphics/weapon.png").convert_alpha()
+        self.rect = self.walking_animation[0].get_rect(center=(800, 450))
+        self.image = self.walking_animation[0]
+        self.animation_count = 0
+        self.walking_right = False
+        self.walking_left = False
+        self.facing_right = True
         self.speed = 5
+
+    def handle_weapon(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        rel_x, rel_y = mouse_x - self.rect.centerx, mouse_y - self.rect.centery
+        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+        if mouse_x < self.rect.centerx:
+            player_weapon_copy = pygame.transform.flip(self.player_weapon, False, True)
+        else:
+            player_weapon_copy = self.player_weapon
+        player_weapon_copy = pygame.transform.rotate(player_weapon_copy, angle)
+        screen.blit(player_weapon_copy, (self.rect.centerx - int(player_weapon_copy.get_width() / 2),
+                                         self.rect.centery + 5 - int(player_weapon_copy.get_height() / 2)))
+
+    def walking(self):
+        if self.animation_count < 15.0:
+            self.animation_count += 1
+        else:
+            self.animation_count = 0
+        if self.walking_right:
+            self.image = self.walking_animation[self.animation_count // 4]
+            self.walking_right = False
+        elif self.walking_left:
+            self.image = pygame.transform.flip(self.walking_animation[self.animation_count // 4], True, False)
+            self.walking_left = False
+        elif self.facing_right:
+            self.image = self.walking_animation[0]
+        else:
+            self.image = pygame.transform.flip(self.walking_animation[0], True, False)
 
     def player_input(self):
         keys = pygame.key.get_pressed()
@@ -23,44 +110,61 @@ class Player(pygame.sprite.Sprite):
                 self.rect.bottom += self.speed
         if keys[pygame.K_RIGHT]:
             if self.rect.right < screen.get_width():
+                self.walking_right = True
+                self.walking_left = False
+                self.facing_right = True
                 self.rect.right += self.speed
         if keys[pygame.K_LEFT]:
             if self.rect.left > 0:
+                self.walking_right = False
+                self.walking_left = True
+                self.facing_right = False
                 self.rect.left -= self.speed
 
     def update(self):
         self.player_input()
+        self.walking()
+        self.handle_weapon()
 
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('graphics/enemy.png').convert_alpha()
-        self.image.fill((randint(0, 255), randint(0, 255), randint(0, 255), 100), special_flags=pygame.BLEND_ADD)
+        self.walking_animation = [pygame.image.load("graphics/enemy_animation_0.png"),
+                                  pygame.image.load("graphics/enemy_animation_1.png"),
+                                  pygame.image.load("graphics/enemy_animation_2.png"),
+                                  pygame.image.load("graphics/enemy_animation_3.png")]
+        self.image = self.walking_animation[0]
+        self.animation_count = 0
         if randint(0, 2):
-            self.rect = self.image.get_rect(center=(random.choice([-50, screen.get_width() + 50]), randint(0, screen.get_height())))
+            self.rect = self.image.get_rect(
+                center=(random.choice([-50, screen.get_width() + 50]), randint(0, screen.get_height())))
         else:
-            self.rect = self.image.get_rect(center=(randint(0, screen.get_width()), random.choice([-50, screen.get_height() + 50])))
+            self.rect = self.image.get_rect(
+                center=(randint(0, screen.get_width()), random.choice([-50, screen.get_height() + 50])))
         self.movement_speed = 2
 
     def move_toward_player(self):
-        if player.sprite.rect.centerx - self.rect.centerx != 0:
-            if player.sprite.rect.centerx > self.rect.centerx:
-                self.rect.centerx += self.movement_speed
-            if player.sprite.rect.centerx < self.rect.centerx:
-                self.rect.centerx -= self.movement_speed
-        if player.sprite.rect.centery - self.rect.centery != 0:
-            if player.sprite.rect.centery > self.rect.centery:
-                self.rect.centery += self.movement_speed
-            if player.sprite.rect.centery < self.rect.centery:
-                self.rect.centery -= self.movement_speed
+        if self.animation_count + 1 < 16.0:
+            self.animation_count += 1
+        else:
+            self.animation_count = 0
+        self.image = self.walking_animation[self.animation_count // 4]
         # overcomplicated
-        #dx, dy = player.sprite.rect.centerx - self.rect.centerx, player.sprite.rect.centery - self.rect.centery
-        #dist = math.hypot(dx, dy)
-        #if dist:
+        # dx, dy = player.sprite.rect.centerx - self.rect.centerx, player.sprite.rect.centery - self.rect.centery
+        # dist = math.hypot(dx, dy)
+        # if dist:
         #    dx, dy = dx / dist, dy / dist
         #    self.rect.x += dx * self.movement_speed
         #    self.rect.y += dy * self.movement_speed
+        if player.sprite.rect.centerx > self.rect.centerx:
+            self.rect.centerx += self.movement_speed
+        elif player.sprite.rect.centerx < self.rect.centerx:
+            self.rect.centerx -= self.movement_speed
+        if player.sprite.rect.centery > self.rect.centery:
+            self.rect.centery += self.movement_speed
+        elif player.sprite.rect.centery < self.rect.centery:
+            self.rect.centery -= self.movement_speed
 
     def prevent_overlap(self):
         for enemy in enemies:
@@ -76,45 +180,25 @@ class Enemy(pygame.sprite.Sprite):
                             self.rect.centery -= self.movement_speed
                         if enemy.rect.centery < self.rect.centery:
                             self.rect.centery += self.movement_speed
-                    #overcomplicated
-                    #dx, dy = enemy.rect.centerx - self.rect.centerx, enemy.rect.centery - self.rect.centery
-                    #dist = math.hypot(dx, dy)
-                    #if dist:
-                    #    if self.rect.centerx > enemy.rect.centerx:
-                    #        dx /= dist
-                    #        self.rect.x -= dx * 2 * self.movement_speed
-                    #        enemy.rect.x += dx * enemy.movement_speed
-                    #    if self.rect.centery > enemy.rect.centery:
-                    #        dy /= dist
-                    #        self.rect.y -= dy * 2 * self.movement_speed
-                    #        enemy.rect.y += dy * enemy.movement_speed
+
+    def suicide(self):
+        for bullet in bullets:
+            if self.rect.colliderect(bullet.rect):
+                bullet.kill()
+                self.kill()
+                break
 
     def update(self):
         self.move_toward_player()
         self.prevent_overlap()
+        self.suicide()
 
 
-def enemy_hit():
-    if pygame.sprite.spritecollide(player.sprite, enemies, False):
-        enemies.empty()
-        player.remove()
-        return False
-    return True
-
-
-pygame.init()
-screen = pygame.display.set_mode((1600, 900))
-pygame.display.set_caption('Dzikie fotele w twojej okolicy')
-clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
 game_active = False
 
 player = pygame.sprite.GroupSingle()
 enemies = pygame.sprite.Group()
-
-# Intro screen
-game_message = test_font.render('Press space to run', False, 'black')
-game_message_rect = game_message.get_rect(center=(800, 600))
+bullets = pygame.sprite.Group()
 
 # Enemy spawn timer
 spawn_timer = pygame.USEREVENT + 1
@@ -132,6 +216,8 @@ while True:
                 # spawn enemy
                 if len(enemies) < max_enemies:
                     enemies.add(Enemy())
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                bullets.add(PlayerBullet())
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 # game start
@@ -145,6 +231,8 @@ while True:
         player.update()
         enemies.draw(screen)
         enemies.update()
+        bullets.draw(screen)
+        bullets.update()
         game_active = enemy_hit()
     else:
         # menu
